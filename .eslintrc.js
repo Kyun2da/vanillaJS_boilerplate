36a7088c8fb30603d3a3365362355f4dfe22aa86
Kyun Heo <kyun2dot@gmail.com>
2021-07-25 01:11:04 +0900

    feat : 기본 린트 설정

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+  },
+};
